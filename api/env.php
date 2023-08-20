0a6bb012bb5e9a48c05e2cf97794bc0b6b8bed0c
Jhomar Altesin <altesin.jhomar@gmail.com>
2023-08-21 04:55:50 +0800

    hosting

--- api/env.php
+++ api/env.php
@@ -7,10 +7,19 @@
  * prod
  */
 
- define("DB_HOST", "localhost");
- define("DB_USER", "root");
- define("DB_PASSWORD", "");
+//  define("DB_HOST", "localhost");
+//  define("DB_USER", "root");
+//  define("DB_PASSWORD", "");
+//  /**
+//   * @var change DB_NAME
+//   */
+//  define("DB_NAME", "db_timer");
+
+
+ define("DB_HOST", "");
+ define("DB_USER", "id21168045_jhom0021");
+ define("DB_PASSWORD", "TaskTimer.1");
  /**
   * @var change DB_NAME
   */
- define("DB_NAME", "db_timer");
+ define("DB_NAME", "id21168045_timer");
